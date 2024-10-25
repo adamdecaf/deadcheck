3ea9f50d16a0bd6e7995791a6346bf0a51675290
Adam Shannon <adamkshannon@gmail.com>
2024-10-25 17:10:08 -0500

    crontab: add FormatDuration

--- internal/crontab/crontab.go
+++ internal/crontab/crontab.go
@@ -10,3 +10,47 @@ func FormatTime(when time.Time) string {
 	// minute   hour   day (month)   month   day (week)
 	return fmt.Sprintf("%d %d %d %d %d", when.Minute(), when.Hour(), when.Day(), when.Month(), when.Weekday())
 }
+
+// FormatDuration converts a time.Duration into a crontab schedule.
+// Not all durations are possible to represnet as a crontab schedule.
+//
+// Set seconds true to include seconds as part of the crontab schedule.
+// Not all crontab parsers support seconds.
+func FormatDuration(dur time.Duration, seconds bool) string {
+	s, m, h, d := "0", "0", "*", "*"
+
+	days := int(dur.Truncate(24*time.Hour).Hours()) / 24
+	if days >= 1 {
+		days += 1 // account for Truncate
+
+		d = fmt.Sprintf("1/%d", days)
+	}
+
+	hours := int(dur.Truncate(time.Hour).Hours()) % 24
+	if hours >= 1 {
+		if days >= 1 {
+			h = fmt.Sprintf("%d", hours)
+		} else {
+			h = fmt.Sprintf("1/%d", hours)
+		}
+	}
+
+	mins := int(dur.Truncate(time.Minute).Minutes()) % 60
+	if mins >= 1 {
+		if hours > 1 {
+			m = fmt.Sprintf("%d", mins)
+		} else {
+			m = fmt.Sprintf("1/%d", mins)
+		}
+	}
+
+	secs := int(dur.Seconds()) % 60
+	if secs >= 1 {
+		s = fmt.Sprintf("1/%d", secs)
+	}
+
+	if seconds {
+		return fmt.Sprintf("%s %s %s %s * *", s, m, h, d)
+	}
+	return fmt.Sprintf("%s %s %s * *", m, h, d)
+}
--- internal/crontab/crontab_test.go
+++ internal/crontab/crontab_test.go
@@ -12,3 +12,27 @@ func TestFormatTime(t *testing.T) {
 	when := time.Date(2024, time.October, 11, 13, 15, 38, 0, loc)
 	require.Equal(t, "15 13 11 10 5", FormatTime(when))
 }
+
+func TestFormatDuration(t *testing.T) {
+	includeSeconds := true
+
+	got := FormatDuration(30*time.Minute, includeSeconds)
+	require.Equal(t, "0 1/30 * * * *", got)
+
+	got = FormatDuration(127*time.Minute, includeSeconds)
+	require.Equal(t, "0 7 1/2 * * *", got)
+
+	got = FormatDuration(time.Hour, includeSeconds)
+	require.Equal(t, "0 0 1/1 * * *", got)
+
+	got = FormatDuration(12*time.Hour, includeSeconds)
+	require.Equal(t, "0 0 1/12 * * *", got)
+
+	// Over 24h isn't really supported by crontab
+	got = FormatDuration(30*time.Hour, includeSeconds)
+	require.Equal(t, "0 0 6 1/2 * *", got) // this is really every 48hrs
+
+	// complex cases
+	got = FormatDuration(3*time.Hour+30*time.Minute, includeSeconds)
+	require.Equal(t, "0 30 1/3 * * *", got)
+}
